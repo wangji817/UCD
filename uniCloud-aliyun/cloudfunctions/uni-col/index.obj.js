b6565363ba12d58f70a508ec433807e68836a131
wangji817 <526153861@qq.com>
2023-02-02 16:36:26 +0800

    更新

--- uniCloud-aliyun/cloudfunctions/uni-col/index.obj.js
+++ uniCloud-aliyun/cloudfunctions/uni-col/index.obj.js
@@ -1,6 +1,8 @@
+// 云对象教程: https://uniapp.dcloud.net.cn/uniCloud/cloud-obj
+// jsdoc语法提示教程：https://ask.dcloud.net.cn/docs/#//ask.dcloud.net.cn/article/129
 module.exports = {
-	say( text ) {
-		if ( text ) {
+	say(text) {
+		if (text) {
 			return {
 				code: 200,
 				message: `hello my text is ${text}`
